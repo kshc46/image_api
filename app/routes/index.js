e93ea591590787272c3ed838da8f4e646c773f77
Kyle <kshc46@mail.missouri.edu>
2016-05-09 23:13:01 +0000

    initial commit

---
+++ app/routes/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var shortener = require(process.cwd() + '/app/controllers/shortener.js');
+
+module.exports = function (app, db) {
+    
+    app.route('/')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/public/index.html');
+        });
+        
+    app.route('/new')
+        .get(function(req,res){
+            res.sendFile(process.cwd() + '/public/index.html', {
+                err: "Error: Need a proper URL"
+            });
+        });
+
+    app.route('/google')
+        .get(function (req, res) {
+            res.sendFile(process.cwd() + '/public/test.html');
+        });
+};
---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express'),
+    mongo = require('mongoskin'),
+    routes = require('./app/routes/index.js'),
+    shortener = require('./app/controllers/shortener.js'),
+    http = require('http');
+    
+var app = express();
+
+var db = mongo.db(process.env.MONGODB_URI || 'mongodb://tester:testerpass@ds017672.mlab.com:17672/short-url')
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+db.createCollection("urls", {
+    capped: true,
+    size: 5242880,
+    max: 5000
+});
+
+routes(app, db);
+shortener(app, db);
+
+var port = Number(process.env.PORT || 8080);
+app.listen(port, function () {
+    console.log('Listening on port' + port);
+});
+    
