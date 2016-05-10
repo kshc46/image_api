4e3d663dc5890537e002038b3d8f23ea0a88d388
Kyle <kshc46@mail.missouri.edu>
2016-05-10 01:39:35 +0000

    updates, server & getter

--- app/controllers/getter.js
+++ app/controllers/getter.js
@@ -1,13 +1,20 @@
 'use strict';
 
 function getter(app, db){
-    var collection = db.collection('imageStuff');
-    var collection = db.collection('searchInfo');
+    var imageCollection = db.collection('imageCollection');
+    var searchCollection = db.collection('searchCollection');
+    var pass = require(process.cwd() + '/public/pass.js');
         
     app.get('/api/imagesearch/:URLarg*', getParameterByName);
     
     function imageAPICall(){
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=449d7d0f088db31263496d4ed169f548&units=" + units[unitID] + "&format=json", function(data) {
+        var password = pass();
+        var offset = getParameterByName('offset');
+        var query = getParameterByName('search');
+        searchAdd(query)
+        $.getJSON("https://www.googleapis.com/customsearch/v1?key=" + password[0] + "&cx=" + password[1] + "&searchType=image&q=" + query + "&start=" + offset, function(data) {
+            
+            
             $(".condition").html(data.weather[0].main)
             $(".location").html(data.name)
             $(".temp").html(Math.floor(data.main.temp) + '&deg; ' + showUnit[unitID])
@@ -15,9 +22,9 @@ function getter(app, db){
         });
     };
     
-    //Get parameters from URL
-    function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
+    //Get parameters from URL, http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+    function getParameterByName(req, name) {
+        var url = req.URL;
         name = name.replace(/[\[\]]/g, "\\$&");
         var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
             results = regex.exec(url);
@@ -26,8 +33,40 @@ function getter(app, db){
         return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
     
-    //Define search API parameters
-    var foo = getParameterByName('foo');
+    //Write image results to DB
+    function write(data){
+        
+    }
+    
+    //Write search to DB
+    function searchAdd(query){
+        var date = new Date();
+        searchCollection.update(
+           { _id: 1 },
+           {
+             $push: {
+                search: {
+                   $each: [ {term: query, when: date.toUTCString()} ],
+                   $position: 0
+                }
+             }
+           }
+        )
+        searchCollection.find({_id : 1}).toArray(function(err,data){
+            if (err) {
+                console.log("Error: ", err)
+            };
+            if (data.search.length > 10) {
+                dbPop();
+            }
+        })
+    }
+    
+    //Removes last from search db array if needed
+    function dbPop() {
+        searchCollection.update( { _id: 1 }, { $pop: { search: 9 } } ) // or search : 1???
+    }
+
     
     
 }
--- server.js
+++ server.js
@@ -4,16 +4,24 @@ var express = require('express'),
     mongo = require('mongoskin'),
     routes = require('./app/routes/index.js'),
     getter = require('./app/controllers/getter.js'),
-    http = require('http');
-    
-var app = express();
+    pass = require(process.cwd() + '/public/pass.js');
 
-var db = mongo.db(process.env.MONGODB_URI || 'mongodb://USER:PASS@etc....')
+var app = express();
+var password = pass();
+
+
+var db = mongo.db(process.env.MONGODB_URI || password[2]);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-db.createCollection("urls", {
+db.createCollection("imageCollection", {
+    capped: true,
+    size: 5242880,
+    max: 5000
+});
+
+db.createCollection("searchCollection", {
     capped: true,
     size: 5242880,
     max: 5000
