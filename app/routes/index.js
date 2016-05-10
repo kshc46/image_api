649038fcb50c9463c0436e06e8f24533842fa0fa
Kyle <kshc46@mail.missouri.edu>
2016-05-10 02:09:36 +0000

    first shot

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,11 +9,4 @@ module.exports = function (app, db) {
             res.sendFile(process.cwd() + '/public/index.html');
         });
         
-    app.route('/api/imagesearch/')
-        .get(function(req,res){
-            res.sendFile(process.cwd() + '/public/index.html', {
-                err: "Error: Need a proper URL"
-            });
-        });
-
 };
